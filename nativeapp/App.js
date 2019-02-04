fa03d6283e3ef497b4591b13c71e580d3031e762
warunika <warunikaw6@gmail.com>
2019-02-04 15:47:22 -0500

    Fix indents

--- nativeapp/App.js
+++ nativeapp/App.js
@@ -1,61 +1,61 @@
 import React from 'react';
 import { Button, View, Text } from 'react-native';
-import { createAppContainer, createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Button from '@material-ui/core/Button';
 
 class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('Details')}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Manage clients and shifts</Text>
+                <Button
+                    color="primary"
+                    onPress={() => this.props.navigation.navigate('Details')}
+                />
+            </View>
+        );
+    }
 }
 
 class DetailsScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.push('Details')}
-        />
-        <Button
-          title="Go to Home"
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-        <Button
-          title="Go back"
-          onPress={() => this.props.navigation.goBack()}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Details Screen</Text>
+                <Button
+                    title="Go to Details... again"
+                    onPress={() => this.props.navigation.push('Details')}
+                />
+                <Button
+                    title="Go to Home"
+                    onPress={() => this.props.navigation.navigate('Home')}
+                />
+                <Button
+                    title="Go back"
+                    onPress={() => this.props.navigation.goBack()}
+                />
+            </View>
+        );
+    }
 }
 
 const RootStack = createStackNavigator(
-  {
+{
     Home: {
-      screen: HomeScreen,
+        screen: HomeScreen,
     },
     Details: {
-      screen: DetailsScreen,
+        screen: DetailsScreen,
     },
-  },
-  {
+},
+{
     initialRouteName: 'Home',
-  }
-);
+});
 
 const AppContainer = createAppContainer(RootStack);
 
 export default class App extends React.Component {
-  render() {
-    return <AppContainer />;
-  }
+    render() {
+        return <AppContainer />;
+    }
 }
