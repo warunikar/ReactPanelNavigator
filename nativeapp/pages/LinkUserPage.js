ddc8c72a3fa4805edd216e0a79fc4bfd3f9a6657
warunika <warunikaw6@gmail.com>
2019-02-04 19:27:53 -0500

    Render users

--- nativeapp/pages/LinkUserPage.js
+++ nativeapp/pages/LinkUserPage.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, View, Text, FlatList } from 'react-native';
 import localStrings from "../resources/localStrings";
 
 class LinkUserPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			usersList: [
+				"Jake Peralta",
+				"Terry Jeffords",
+				"Amy Santiago",
+				"Rosa Diaz"
+			]
+		};
+	};
+
+	prepItems = (itemsList) => (
+		itemsList.map((item) => ({key: item}))
+	);
+
     render() {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>{localStrings.listOfUsers}</Text>
+                <FlatList
+                  data={this.prepItems(this.state.usersList)}
+                  renderItem={({item}) => <Text>{item.key}</Text>}
+                />
                 <Button
                     title={localStrings.addNewUserTitle}
                     onPress={() => this.props.navigation.push('AddNewUser')}
